5973d2371ef8dd7fec88dce3363f922ad6c7389b
Angel <angel.aven.c@gmail.com>
2021-07-25 03:37:03 -0500

    Request one ASIN number and request Similar poducts

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
 const asinValidationNumber = require('../utils/middleware/asinValidationNumber')
 const productsMock = require('../utils/data/fakeScrappingProducts')
 
@@ -15,7 +16,7 @@ function productsAPI(app){
         }
     })
 
-    router.get('/:productASIN', (req, res, next) => {
+    router.get('/fake/:productASIN', (req, res, next) => {
         const { productASIN } = req.params
         const regex = new RegExp(/\w{10}/)
         let productFound = ''
@@ -40,14 +41,59 @@ function productsAPI(app){
 
     })
 
-    router.post('/', asinValidationNumber(), (req, res, next) => {
+    router.post('/fake', asinValidationNumber(), (req, res, next) => {
         const { body: productRequested } = req
 
-            try {
-                res.status(201).json(productsMock)
-            } catch (e) {
-                next(e)
+        try {
+            res.status(201).json(productsMock)
+        } catch (e) {
+            next(e)
+        }
+    })
+
+    router.get('/:productASIN', (req, res, next) => {
+        const { productASIN } = req.params
+        const url = 'https://www.amazon.com.mx/dp/' + productASIN
+
+        async function run(){
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+
+            async function getData(){
+                await page.goto(`${url}`)
+
+                const data = await page.evaluate((productASIN) => {
+                    const baseURL = 'https://www.amazon.com.mx/'
+                    const similarProducts = []
+                    const product = []
+                    const $similarProducts = document.querySelectorAll('.comparison_table_image_row th')
+
+                    $similarProducts.forEach($product => {
+                        if ($product.getAttribute('data-asin').trim() !== productASIN.trim()){
+                            similarProducts.push({
+                                asin_number: $product.getAttribute('data-asin'),
+                                product_name: $product.querySelector('.a-size-base').textContent,
+                                product_url: baseURL + $product.querySelector('.a-link-normal').getAttribute("href"),
+                                product_image: $product.querySelector('img').getAttribute('src')
+                            })
+                        }
+                    })
+
+                    return {
+                        product,
+                        similarProducts
+                    }
+                }, productASIN)
+
+                return data
             }
+
+            res.status(200).json(await getData())
+
+            await browser.close()
+        }
+
+        run()
     })
 }
 
